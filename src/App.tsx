03b61b0c45ff248fc9d2223362c94275b1d1ca85
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-10-19 22:25:13 +0900

    account generate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Account, NetworkType } from 'symbol-sdk'
 
 function App() {
+  const accountCreate = () => {
+    const account = Account.generateNewAccount(NetworkType.TEST_NET)
+    console.log(
+      'Your new account address is:',
+      account.address.pretty(),
+      'and its private key',
+      account.privateKey
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={accountCreate}>アカウントの作成</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
