b92c3222b80d90d30fd74925c2bc0d6a05eef910
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-10-25 22:53:38 +0900

    next input area

--- src/component/CreateFromPrivateKey.tsx
+++ src/component/CreateFromPrivateKey.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Account, NetworkType } from 'symbol-sdk'
 
 const CreateFromPrivateKey = () => {
+  const [privateKey, setPrivateKey] = useState('')
+  console.log('秘密鍵', privateKey)
+
+  const sampleUseState = () => {
+    setPrivateKey(
+      '7B20E0615755D6EEDA0DAB45E5D8A4331EC603F8702D7F4E6171FB81CF83CF78'
+    )
+  }
+
   const accountCreateFromPrivateKey = () => {
     const account = Account.createFromPrivateKey(
-      '7B20E0615755D6EEDA0DAB45E5D8A4331EC603F8702D7F4E6171FB81CF83CF78',
+      privateKey,
       NetworkType.TEST_NET
     )
     console.log(
@@ -18,8 +27,9 @@ const CreateFromPrivateKey = () => {
     <div>
       <button onClick={accountCreateFromPrivateKey}>
         秘密鍵からアカウントを作成する
-      </button>{' '}
-      {/* ここが追加されるよ！！ */}
+      </button>
+      <br />
+      <button onClick={sampleUseState}>useStateを試してみる</button>
     </div>
   )
 }
