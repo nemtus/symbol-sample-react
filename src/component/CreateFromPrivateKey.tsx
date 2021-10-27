7649ec8a02b43f86132eb2169ec401172d6ad859
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-10-27 21:44:23 +0900

    モザイクやインポータンス取得

--- src/component/CreateFromPrivateKey.tsx
+++ src/component/CreateFromPrivateKey.tsx
@@ -4,13 +4,33 @@ import {
   NetworkType,
   Address,
   RepositoryFactoryHttp,
+  Mosaic,
 } from 'symbol-sdk'
 
 const CreateFromPrivateKey = () => {
   const [privateKey, setPrivateKey] = useState('')
   const [address, setAddress] = useState('')
   const [publicKey, setPublicKey] = useState('')
-  console.log('秘密鍵', privateKey)
+  const [mosaics, setMosaics] = useState<Mosaic[]>([])
+  const [importance, setImportance] = useState({
+    lower: 0,
+    higher: 0,
+  })
+
+  const mosaicList = () => {
+    const items = []
+    for (let i = 0; i < mosaics.length; i++) {
+      items.push(
+        <li key={mosaics[i].id.id.higher}>
+          モザイクハイター{mosaics[i].amount.higher}<br/>
+          モザイクローワ{mosaics[i].amount.lower}<br/>
+          モザイクIDローワ{mosaics[i].id.id.lower}<br/>
+          モザイクIDハイター{mosaics[i].id.id.higher}
+        </li>
+      )
+    }
+    return <ul>{items}</ul>
+  }
 
   const accountInfo = () => {
     const accountAddress = Address.createFromRawAddress(address)
@@ -18,9 +38,13 @@ const CreateFromPrivateKey = () => {
     const repositoryFactory = new RepositoryFactoryHttp(nodeUrl)
     const accountHttp = repositoryFactory.createAccountRepository()
     accountHttp.getAccountInfo(accountAddress).subscribe(
-      (accountInfo) => console.log(accountInfo),
-      (err) => console.error(err),
-    );
+      (accountInfo) => {
+        console.log(accountInfo)
+        setMosaics(accountInfo.mosaics)
+        setImportance(accountInfo.importance)
+      },
+      (err) => console.error(err)
+    )
   }
 
   const accountCreateFromPrivateKey = () => {
@@ -44,6 +68,13 @@ const CreateFromPrivateKey = () => {
       <p>アドレス: {address}</p>
       <p>公開鍵: {publicKey}</p>
       <button onClick={accountInfo}>アカウント情報を取得する</button>
+      {mosaics && importance && (
+        <>
+          {mosaicList()}
+          <p>インポータンスローワー{importance.lower}</p>
+          <p>インポータンスハイター{importance.higher}</p>
+        </>
+      )}
     </div>
   )
 }
