2bbe9731a65d8f35babedb22d8a5f147c9acbf32
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-11-01 22:38:35 +0900

    account page

--- src/component/CreateFromPrivateKey.tsx
+++ src/component/CreateFromPrivateKey.tsx
@@ -19,14 +19,13 @@ const CreateFromPrivateKey = () => {
     higher: 0,
   })
 
-
-
   const mosaicList = () => {
     const items = []
     for (let i = 0; i < mosaics.length; i++) {
       items.push(
         <li key={mosaics[i].id.id.lower}>
-          モザイクID: {mosaics[i].id.id.toHex()}------モザイクの総量: {mosaics[i].amount.toString()}
+          モザイクID: {mosaics[i].id.id.toHex()}<br/>モザイクの総量:{' '}
+          {mosaics[i].amount.toString()}
         </li>
       )
     }
@@ -63,21 +62,30 @@ const CreateFromPrivateKey = () => {
   }
   return (
     <div>
+      <div className="shadow rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+        Account
+      </div>
       <input
         onChange={(e) => setPrivateKey(e.target.value)}
-        className="shadow rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        className="shadow rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline mt-5"
+        placeholder="enter the privateKey"
       />
       <br />
-      <button onClick={accountCreateFromPrivateKey}>
+      <button
+        onClick={accountCreateFromPrivateKey}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
         秘密鍵からアカウントを作成する
       </button>
-      <p>アドレス: {address}</p>
-      <p>公開鍵: {publicKey}</p>
+      <div className="mt-5 shadow rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+        <p>アドレス: {address}</p>
+        <p>公開鍵: {publicKey}</p>
+      </div>
       {mosaics && importance && (
-        <>
+        <div className="mt-5 shadow rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
           {mosaicList()}
           <p>インポータンス: {importance.toString()}</p>
-        </>
+        </div>
       )}
     </div>
   )
--- src/component/sidebar.tsx
+++ src/component/sidebar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import CreateFromPrivateKey from './CreateFromPrivateKey'
+import Symbol from '../images/logo-symbol-color.png'
+import NemTus from '../images/logo-nemtus-color.png'
 
 const SideBar = () => {
   const [isClosed, setClosed] = useState(false)
@@ -16,46 +18,46 @@ const SideBar = () => {
 
           <div className="border-r py-4 flex-grow relative">
             <nav>
-            <ul className="mt-12">
-          <li className="flex w-full justify-between text-white hover:text-gray-500 cursor-pointer items-center mb-6 m-4">
-            <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-              <span className="text-sm  ml-2">Home</span>
-            </div>
-          </li>
-          <li className="flex w-full justify-between text-white hover:text-gray-500 cursor-pointer items-center  m-4">
-            <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-sm  ml-2">Account</span>
-            </div>
-          </li>
-        </ul>
+              <ul className="mt-12">
+                <li className="flex w-full justify-between text-white hover:text-gray-500 cursor-pointer items-center mb-6 m-4">
+                  <div className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                      />
+                    </svg>
+                    <span className="text-sm  ml-2">Home</span>
+                  </div>
+                </li>
+                <li className="flex w-full justify-between text-white hover:text-gray-500 cursor-pointer items-center  m-4">
+                  <div className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <span className="text-sm  ml-2">Account</span>
+                  </div>
+                </li>
+              </ul>
             </nav>
           </div>
         </aside>
@@ -104,9 +106,15 @@ const SideBar = () => {
             </button>
           )}
 
-          <div className="flex flex-grow items-center justify-between px-3">
-            <h1 className="text-lg">Home</h1>
-            <button className="text-blue-700 underline">Log in</button>
+          <div className="flex flex-grow items-center px-3">
+            <h1 className="text-lg col-start-1 col-end-3">Home</h1>
+            <div className="flex-grow"></div>
+            <a href="https://docs.symbolplatform.com/ja/" target="_blank" rel="noopener noreferrer">
+              <img src={Symbol} alt="symbol" className="col-end-7 col-span-2" />
+            </a>
+            <a href="https://nemtus.com/" target="_blank" rel="noopener noreferrer">
+              <img src={NemTus} alt="nemtus" className="w-6 m-2" />
+            </a>
           </div>
         </header>
         <CreateFromPrivateKey></CreateFromPrivateKey>
